788e64879f1c93f5addd2aa3876473ae5cb030cf
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-11-11 14:56:53 +1000

    Added files for views and controllers

--- public/js/app-routes.js
+++ public/js/app-routes.js
@@ -7,17 +7,20 @@ angular.module("manga.routes", []).config(
     // news page state
     .state("news", {
       url: "/news",
-      templateUrl: "./views/news.html"
+      templateUrl: "./views/news.html",
+      controller: "NewsCtrl as news"
     })
     // list of mangakas
     .state("authors", {
       url: "/authors",
-      templateUrl: "./views/authors.html"
+      templateUrl: "./views/authors.html",
+      controller: "AuthorsCtrl as authors"
     })
     // list of series
     .state("series", {
       url: "/series",
-      templateUrl: "./views/series.html"
+      templateUrl: "./views/series.html",
+      controller: "SeriesCtrl as series"
     });
   }]
 );
