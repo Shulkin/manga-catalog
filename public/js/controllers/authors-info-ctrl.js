a5b7bfda7aa0495424e017bebb8b356987d886bf
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-11-16 12:51:34 +1000

    Added layout to authors info card

--- public/js/controllers/authors-info-ctrl.js
+++ public/js/controllers/authors-info-ctrl.js
@@ -11,22 +11,10 @@ angular.module("authors.info.ctrl", [])
   function init() {
     Authors.get(id)
     .success(function(author) {
-      /*
-      name: String,
-      gender: {
-        type: String,
-        enum: ["Male", "Female"],
-        default: "Male"
-      },
-      birthDate: Date,
-      // define ref to populate field on find
-      series: [{type: mongoose.Schema.Types.ObjectId, ref: "Manga"}]
-      */
       vm.name = author.name;
       vm.gender = author.gender;
       vm.birthDate = author.birthDate;
-      // ===
-      vm.json = author; // delete later!
+      vm.series = author.series;
     })
     .error(function(err) {
       console.log("Error " + err);
