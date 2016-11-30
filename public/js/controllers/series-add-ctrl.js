6d19886a9c1873134cace861b1b27b4ef455f39b
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-11-30 14:22:53 +1000

    Fixed layout on add manga screen

--- public/js/controllers/series-add-ctrl.js
+++ public/js/controllers/series-add-ctrl.js
@@ -3,12 +3,13 @@ angular.module("series.add.ctrl", [])
   Genres, Authors) {
   // === Variables ===
   var vm = this;
-  vm.yearFormat = 'yyyy'; // year-only
-  vm.dateOptions = {
+  vm.format = 'yyyy'; // year-only
+  vm.options = {
     formatYear: 'yyyy',
     startingDay: 1,
     minMode: 'year'
   };
+  vm.status = {opened: false};
   // === Private ===
   function getAllGenres(callback) {
     Genres.getAll()
@@ -38,6 +39,9 @@ angular.module("series.add.ctrl", [])
     vm.allGenres = [];
     getAllGenres(function(data) {
       vm.allGenres = data;
+      if (vm.allGenres.length > 0) {
+        vm.newGenre = vm.allGenres[0]._id;
+      }
     });
     // load all authors
     vm.allAuthors = [];
@@ -52,7 +56,40 @@ angular.module("series.add.ctrl", [])
   // === Start module ===
   init();
   // === Public ===
+  vm.open = function($event) {
+    vm.status.opened = true;
+  }
+  vm.addGenre = function(genreId) {
+    var obj = {_id: genreId}; // without name
+    // search genre name in allGenres array
+    for (var i = 0; i < vm.allGenres.length; i++) {
+      if (vm.allGenres[i]._id === genreId) {
+        obj = { // fill all fields
+          _id: vm.allGenres[i]._id,
+          name: vm.allGenres[i].name
+        }; // find only first entry
+        break;
+      }
+    }
+    vm.genre.push(obj);
+  }
+  vm.removeGenre = function(genreId) {
+    var i = vm.genre.length;
+    while (i--) { // loop backwards
+      if (vm.genre[i]._id === genreId) {
+        vm.genre.splice(i, 1);
+      }
+    }
+  }
   vm.save = function() {
     console.log("SeriesAddCtrl.save()");
+    var manga = {
+      title: vm.title,
+      description: vm.description,
+      year: vm.date.getFullYear(),
+      genre: Genres.extractIds(vm.genre),
+      author: vm.authorId
+    };
+    console.log("[SeriesAddCtrl.save] manga = " + JSON.stringify(manga));
   }
 });
