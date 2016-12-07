ada3fcef5241697104a6c13c3f41b6ed8c95dd58
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-12-07 12:40:13 +1000

    Removed debug from auth service

--- public/src/js/controllers/auth-ctrl.js
+++ public/src/js/controllers/auth-ctrl.js
@@ -6,22 +6,16 @@ angular.module("manga.auth.ctrl", [])
   "Auth", "$scope", "$state", function(Auth, $scope, $state) {
     $scope.user = {};
     $scope.register = function() {
-      console.log("[AuthCtrl] Ready to register...");
       Auth.register($scope.user).error(function(error) {
-        console.log("[AuthCtrl.Register] Error: " + JSON.stringify(error));
         $scope.error = error;
       }).then(function() {
-        console.log("[AuthCtrl.Register] Success, go home!");
         $state.go("home");
       });
     };
     $scope.logIn = function() {
-      console.log("[AuthCtrl] Ready to login...");
       Auth.logIn($scope.user).error(function(error) {
-        console.log("[AuthCtrl.Login] Error: " + JSON.stringify(error));
         $scope.error = error;
       }).then(function() {
-        console.log("[AuthCtrl.Login] Success, go home!");
         $state.go("home");
       });
     };
--- public/src/js/services/auth.js
+++ public/src/js/services/auth.js
@@ -28,16 +28,12 @@ angular.module("manga.auth", [])
     }
   };
   auth.register = function(user) {
-    console.log("[AuthService] Send http POST register: " + JSON.stringify(user));
     return $http.post("/auth/register", user).success(function(data) {
-      console.log("[AuthService.Register] Success: " + JSON.stringify(data));
       auth.saveToken(data.token);
     });
   };
   auth.logIn = function(user) {
-    console.log("[AuthService] Send http POST login: " + JSON.stringify(user));
     return $http.post("/auth/login", user).success(function(data) {
-      console.log("[AuthService.Login] Success: " + JSON.stringify(data));
       auth.saveToken(data.token);
     });
   };
