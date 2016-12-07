2b6085127071c35b3093ca3e1acff59dd0b2b497
Evgeny Shulkin <evgeny.shulkin@gmail.com>
2016-12-08 09:59:35 +1000

    Removed comments from app-routes

--- public/src/js/app-routes.js
+++ public/src/js/app-routes.js
@@ -56,49 +56,5 @@ angular.module("manga.routes", []).config(
       templateUrl: "./views/register.html",
       controller: "AuthCtrl"
     });
-    /*
-    // news page state
-    .state("news", {
-      url: "/news",
-      templateUrl: "./views/news.html",
-      controller: "NewsCtrl as news"
-    })
-    // list of mangakas
-    .state("authors", {
-      url: "/authors",
-      templateUrl: "./views/authors.html",
-      controller: "AuthorsCtrl as authors"
-    })
-    // mangaka info card (not nested!)
-    .state("authors-info", {
-      url: "/authors/:id",
-      templateUrl: "./views/authors-info.html",
-      controller: "AuthorsInfoCtrl as author"
-    })
-    // create new author screen
-    .state("authors-create", {
-      url: "/add/author",
-      templateUrl: "./views/authors-create.html",
-      controller: "AuthorsAddCtrl as author"
-    })
-    // list of series
-    .state("series", {
-      url: "/series",
-      templateUrl: "./views/series.html",
-      controller: "SeriesCtrl as series"
-    })
-    // series info card (not nested!)
-    .state("series-info", {
-      url: "/series/:id",
-      templateUrl: "./views/series-info.html",
-      controller: "SeriesInfoCtrl as manga"
-    })
-    // create new manga screen
-    .state("series-create", {
-      url: "/add/manga",
-      templateUrl: "./views/series-create.html",
-      controller: "SeriesAddCtrl as manga"
-    });
-    */
   }]
 );
